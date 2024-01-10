6343682891b67356e6dcfb80ea1623d9e2fa4472
yehuohan <yehuohan@qq.com>
2024-01-10 23:47:58 +0800

    feat(libvktor): add ResourceState::add and ResourceState::request

--- src/libvktor/vktor/base/resource_cache.hpp
+++ src/libvktor/vktor/base/resource_cache.hpp
@@ -6,24 +6,60 @@
 #include "vktor/core/render_pass.hpp"
 #include "vktor/core/sampler.hpp"
 #include "vktor/core/shader_module.hpp"
+#include <functional>
 #include <mutex>
 
 NAMESPACE_BEGIN(vkt)
 
 using namespace core;
 
-#define RESOURCE_STATE_LOCKER(rs) std::lock_guard<std::mutex> guard((rs).mtx)
+#define LOCK_RESOURCE_STATE(rs) std::lock_guard<std::mutex> guard((rs).mtx)
 
 template <typename T>
-struct ResourceState {
+class ResourceState {
+private:
     HashMap<size_t, T> map{};
     std::mutex mtx;
 
+public:
     inline HashMap<size_t, T>::iterator find(size_t key) {
         return map.find(key);
     }
+
     inline bool found(const HashMap<size_t, T>::iterator& item) {
         return item != map.end();
+    }
+
+    /**
+     * @brief Add resource
+     *
+     * This returns the iterator returned from HashMap::insert() directly.
+     * So you should make sure the insertion is going to success or fail.
+     */
+    inline HashMap<size_t, T>::iterator add(size_t key, T&& value) {
+        return map.insert({key, std::move(value)}).first;
+    }
+
+    /**
+     * @brief Request resource that stored in `map`
+     */
+    inline Res<Ref<T>> request(size_t key, std::function<Res<T>()> fn) {
+        LOCK_RESOURCE_STATE(*this);
+
+        T* ptr;
+        {
+            auto item = this->find(key);
+            if (this->found(item)) {
+                ptr = &item->second;
+            } else {
+                auto res = fn();
+                OnErr(res);
+                auto iter = this->add(key, res.unwrap());
+                ptr = &iter->second;
+            }
+        }
+
+        return Ok(newRef(*ptr));
     }
 };
 
