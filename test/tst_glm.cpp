c782c84a1a6d487eaa98144c287f77c5deb8a515
yehuohan <yehuohan@qq.com>
2024-04-10 23:09:39 +0800

    fix(test): add compute_mvp and compute_jitter

--- test/tst_glm.cpp
+++ test/tst_glm.cpp
@@ -2,7 +2,8 @@
 #include <glm/gtx/io.hpp>
 #include <iostream>
 
-int main() {
+void compute_mvp() {
+    std::cout << "===============================================================================\n";
     glm::mat4 mmodel = glm::mat4(1.0);
     // mmodel = glm::translate(mmodel, glm::vec3(1.0f, 1.0f, 1.0f));
     // mmodel = glm::scale(mmodel, glm::vec3(5.0f, 5.0f, 5.0f));
@@ -10,16 +11,39 @@ int main() {
 
     glm::mat4 mview = glm::lookAt(glm::vec3(0.0, 0.0, 5.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
 
-    // (znear, zfar): 距离eye的距离，不是坐标，如果平面在eys后面，需要为正值
+    // (znear, zfar): 距离eye的距离，不是坐标
     glm::mat4 mproj = glm::perspective(glm::radians(45.0), 1.0 / 1.0, 0.1, 100.0);
     // glm::mat4 mproj = glm::ortho(-160.0f, 160.0f, -160.0f, 160.0f, -1.0f, 10.0f);
 
-    glm::mat4 m = mproj * mview * mmodel;
-    std::cout << m << std::endl;
+    glm::mat4 mvp = mproj * mview * mmodel;
+    std::cout << "mvp: " << mvp << std::endl;
 
-    glm::vec4 p = m * glm::vec4(0.2, 0.2, 0.5, 1.0);
-    std::cout << p << std::endl;
+    glm::vec4 pos = mvp * glm::vec4(0.2, 0.2, 0.5, 1.0);
+    std::cout << "pos: " << pos << std::endl;
 
-    p = m * glm::vec4(0.5, 0.5, -0.5, 1.0);
-    std::cout << p << std::endl;
+    pos = mvp * glm::vec4(0.2, 0.2, -0.5, 1.0);
+    std::cout << "pos: " << pos << std::endl;
+}
+
+void compute_jitter() {
+    std::cout << "===============================================================================\n";
+    glm::mat4 mview = glm::lookAt(glm::vec3(0.0, 0.0, 5.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+    glm::mat4 mproj = glm::perspective(glm::radians(45.0), 1.0 / 1.0, 0.1, 100.0);
+
+    glm::vec4 pos = glm::vec4(0.5, 0.5, 1.0, 1.0);
+    glm::mat4 vp = mproj * mview;
+    std::cout << "vp: " << vp << std::endl;
+    std::cout << "pos: " << vp * pos << std::endl;
+
+    // Apply jitter
+    vp[2][0] += 0.1;
+    vp[2][1] += -0.1;
+
+    std::cout << "vp: " << vp << std::endl;
+    std::cout << "pos: " << vp * pos << std::endl;
+}
+
+int main() {
+    compute_mvp();
+    compute_jitter();
 }
