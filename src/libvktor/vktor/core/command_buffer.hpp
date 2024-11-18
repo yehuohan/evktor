41f16ac97b07873660f2927f2357c067890ab345
yehuohan <yehuohan@qq.com>
2024-11-18 22:55:23 +0800

    fix(libvktor): remove cmdTransitImageLayout, complete pipeline barrier cmds

--- src/libvktor/vktor/core/command_buffer.hpp
+++ src/libvktor/vktor/core/command_buffer.hpp
@@ -200,6 +200,39 @@ public:
                              0,
                              nullptr);
     }
+    inline void cmdMemoryBarrier(VkPipelineStageFlags src_stage,
+                                 VkPipelineStageFlags dst_stage,
+                                 VkAccessFlags src_access,
+                                 VkAccessFlags dst_access,
+                                 VkDependencyFlags flags = 0) const {
+        auto barrier = Itor::MemoryBarrier();
+        barrier.srcAccessMask = src_access;
+        barrier.dstAccessMask = dst_access;
+        vkCmdPipelineBarrier(handle, src_stage, dst_stage, flags, 1, &barrier, 0, nullptr, 0, nullptr);
+    }
+    inline void cmdMemoryBarrier2(const Vector<VkMemoryBarrier2>& barriers, VkDependencyFlags flags = 0) const {
+        auto info = Itor::DependencyInfo();
+        info.dependencyFlags = flags;
+        info.memoryBarrierCount = u32(barriers.size());
+        info.pMemoryBarriers = barriers.data();
+        vkCmdPipelineBarrier2(handle, &info);
+    }
+    inline void cmdMemoryBarrier2(VkPipelineStageFlags2 src_stage,
+                                  VkPipelineStageFlags2 dst_stage,
+                                  VkAccessFlags2 src_access,
+                                  VkAccessFlags2 dst_access,
+                                  VkDependencyFlags flags = 0) const {
+        auto barrier = Itor::MemoryBarrier2();
+        barrier.srcStageMask = src_stage;
+        barrier.dstStageMask = dst_stage;
+        barrier.srcAccessMask = src_access;
+        barrier.dstAccessMask = dst_access;
+        auto info = Itor::DependencyInfo();
+        info.dependencyFlags = flags;
+        info.memoryBarrierCount = 1;
+        info.pMemoryBarriers = &barrier;
+        vkCmdPipelineBarrier2(handle, &info);
+    }
     inline void cmdBufferMemoryBarrier(VkPipelineStageFlags src_stage,
                                        VkPipelineStageFlags dst_stage,
                                        const Vector<VkBufferMemoryBarrier>& barriers,
@@ -214,6 +247,65 @@ public:
                              barriers.data(),
                              0,
                              nullptr);
+    }
+    /**
+     * @brief Buffer memory barrier
+     *
+     * If `size` is 0, then use `buf.size`.
+     */
+    inline void cmdBufferMemoryBarrier(const Buffer& buf,
+                                       VkPipelineStageFlags src_stage,
+                                       VkPipelineStageFlags dst_stage,
+                                       VkAccessFlags src_access,
+                                       VkAccessFlags dst_access,
+                                       VkDeviceSize offset = 0,
+                                       VkDeviceSize size = 0,
+                                       VkDependencyFlags flags = 0) const {
+        auto barrier = Itor::BufferMemoryBarrier();
+        barrier.srcAccessMask = src_access;
+        barrier.dstAccessMask = dst_access;
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.buffer = buf;
+        barrier.offset = offset;
+        barrier.size = size == 0 ? buf.size : size;
+        vkCmdPipelineBarrier(handle, src_stage, dst_stage, flags, 0, nullptr, 1, &barrier, 0, nullptr);
+    }
+    inline void cmdBufferMemoryBarrier2(const Vector<VkBufferMemoryBarrier2>& barriers, VkDependencyFlags flags = 0) const {
+        auto info = Itor::DependencyInfo();
+        info.dependencyFlags = flags;
+        info.bufferMemoryBarrierCount = u32(barriers.size());
+        info.pBufferMemoryBarriers = barriers.data();
+        vkCmdPipelineBarrier2(handle, &info);
+    }
+    /**
+     * @brief Buffer memory barrier
+     *
+     * If `size` is 0, then use `buf.size`.
+     */
+    inline void cmdBufferMemoryBarrier2(const Buffer& buf,
+                                        VkPipelineStageFlags2 src_stage,
+                                        VkPipelineStageFlags2 dst_stage,
+                                        VkAccessFlags2 src_access,
+                                        VkAccessFlags2 dst_access,
+                                        VkDeviceSize offset = 0,
+                                        VkDeviceSize size = 0,
+                                        VkDependencyFlags flags = 0) const {
+        auto barrier = Itor::BufferMemoryBarrier2();
+        barrier.srcStageMask = src_stage;
+        barrier.dstStageMask = dst_stage;
+        barrier.srcAccessMask = src_access;
+        barrier.dstAccessMask = dst_access;
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.buffer = buf;
+        barrier.offset = offset;
+        barrier.size = size == 0 ? buf.size : size;
+        auto info = Itor::DependencyInfo();
+        info.dependencyFlags = flags;
+        info.bufferMemoryBarrierCount = 1;
+        info.pBufferMemoryBarriers = &barrier;
+        vkCmdPipelineBarrier2(handle, &info);
     }
     inline void cmdImageMemoryBarrier(VkPipelineStageFlags src_stage,
                                       VkPipelineStageFlags dst_stage,
@@ -230,13 +322,57 @@ public:
                              u32(barriers.size()),
                              barriers.data());
     }
-    /**
-     * @brief Transit image layout with Arg<Image>::VkImageSubresourceRange
-     */
-    bool cmdTransitImageLayout(const Arg<Image>& img,
-                               VkImageLayout old_layout,
-                               VkImageLayout new_layout,
-                               VkDependencyFlags flags = 0) const;
+    inline void cmdImageMemoryBarrier(const Arg<Image>& img,
+                                      VkPipelineStageFlags src_stage,
+                                      VkPipelineStageFlags dst_stage,
+                                      VkAccessFlags src_access,
+                                      VkAccessFlags dst_access,
+                                      VkImageLayout old_layout,
+                                      VkImageLayout new_layout,
+                                      VkDependencyFlags flags = 0) const {
+        auto barrier = Itor::ImageMemoryBarrier();
+        barrier.srcAccessMask = src_access;
+        barrier.dstAccessMask = dst_access;
+        barrier.oldLayout = old_layout;
+        barrier.newLayout = new_layout;
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.image = img;
+        barrier.subresourceRange = img;
+        vkCmdPipelineBarrier(handle, src_stage, dst_stage, flags, 0, nullptr, 0, nullptr, 1, &barrier);
+    }
+    inline void cmdImageMemoryBarrier2(const Vector<VkImageMemoryBarrier2>& barriers, VkDependencyFlags flags = 0) const {
+        auto info = Itor::DependencyInfo();
+        info.dependencyFlags = flags;
+        info.imageMemoryBarrierCount = u32(barriers.size());
+        info.pImageMemoryBarriers = barriers.data();
+        vkCmdPipelineBarrier2(handle, &info);
+    }
+    inline void cmdImageMemoryBarrier2(const Arg<Image>& img,
+                                       VkPipelineStageFlags2 src_stage,
+                                       VkPipelineStageFlags2 dst_stage,
+                                       VkAccessFlags2 src_access,
+                                       VkAccessFlags2 dst_access,
+                                       VkImageLayout old_layout,
+                                       VkImageLayout new_layout,
+                                       VkDependencyFlags flags = 0) const {
+        auto barrier = Itor::ImageMemoryBarrier2();
+        barrier.srcStageMask = src_stage;
+        barrier.dstStageMask = dst_stage;
+        barrier.srcAccessMask = src_access;
+        barrier.dstAccessMask = dst_access;
+        barrier.oldLayout = old_layout;
+        barrier.newLayout = new_layout;
+        barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+        barrier.image = img;
+        barrier.subresourceRange = img;
+        auto info = Itor::DependencyInfo();
+        info.dependencyFlags = flags;
+        info.imageMemoryBarrierCount = 1;
+        info.pImageMemoryBarriers = &barrier;
+        vkCmdPipelineBarrier2(handle, &info);
+    }
 };
 
 NAMESPACE_END(core)
